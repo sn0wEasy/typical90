b5eda660d91a8611873505acf3a44e73f3c05c48
benicandy <yel.bocsmile@gmail.com>
2021-05-24 10:31:32 +0900

    submitted

---
+++ submission/020.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, n) for(ll i = 0; i < (ll)(n); i++)
+#define rep1(i, n) for(ll i = 1; i < (ll)(n); i++)
+#define all(x) (x).begin(),(x).end()
+#define MOD 1000000007
+#define INF (1e9)
+#define PI (acos(-1))
+#define print(x) cout << x << endl
+#define yes cout << "Yes" << endl
+#define YES cout << "YES" << endl
+#define no cout << "No" << endl
+#define NO cout << "NO" << endl
+
+template<class T> inline bool chmin(T& a, T b) 
+{ if (a > b) { a = b; return true; } return false; }
+
+template<class T> inline bool chmax(T& a, T b) 
+{ if (a < b) { a = b; return true; } return false; }
+
+template<class T> inline auto __lcm(T& a, T& b) 
+{ return a * b / __gcd(a, b); }
+
+template<typename T> void remove(std::vector<T>& vector, unsigned int index)
+{ vector.erase(vector.begin() + index); }
+
+
+int main() {
+
+  ll a, b, c;  cin >> a >> b >> c;
+  ll c_b = 1;
+  rep(i, b) {
+    c_b = c_b*c;
+  }
+  if (a < c_b) {
+    yes;
+  } else {
+    no;
+  }
+
+  return 0;
+}
